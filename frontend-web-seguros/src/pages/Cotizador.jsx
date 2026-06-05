84034ed7d09cb443c2054392143141cedaa5b71a
valenoe <valenoe4@gmail.com>
2026-06-05 08:37:27 -0400

    traje de vuelta la constante regionesComunas y ahora está con todas las comunas

--- frontend-web-seguros/src/pages/Cotizador.jsx
+++ frontend-web-seguros/src/pages/Cotizador.jsx
@@ -7,8 +7,378 @@ import { obtenerSeguros, enviarCotizacion } from "../services/api";
 
 const regionesComunas = {
   "Arica y Parinacota": ["Arica", "Camarones", "Putre", "General Lagos"],
-  Maule: ["Talca", "Constitución", "Curicó", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"],
-  "Metropolitana de Santiago": ["Santiago", "San Miguel", "Ñuñoa", "Providencia", "Las Condes", "Maipú", "La Florida", "Puente Alto"],
+  Tarapacá: [
+    "Iquique",
+    "Alto Hospicio",
+    "Pozo Almonte",
+    "Camiña",
+    "Colchane",
+    "Huara",
+    "Pica",
+  ],
+  Antofagasta: [
+    "Antofagasta",
+    "Mejillones",
+    "Sierra Gorda",
+    "Taltal",
+    "Calama",
+    "Ollagüe",
+    "San Pedro de Atacama",
+    "Tocopilla",
+    "María Elena",
+  ],
+  Atacama: [
+    "Copiapó",
+    "Caldera",
+    "Tierra Amarilla",
+    "Chañaral",
+    "Diego de Almagro",
+    "Vallenar",
+    "Alto del Carmen",
+    "Freirina",
+    "Huasco",
+  ],
+  Coquimbo: [
+    "La Serena",
+    "Coquimbo",
+    "Andacollo",
+    "La Higuera",
+    "Paihuano",
+    "Vicuña",
+    "Illapel",
+    "Canela",
+    "Los Vilos",
+    "Salamanca",
+    "Ovalle",
+    "Combarbalá",
+    "Monte Patria",
+    "Punitaqui",
+    "Río Hurtado",
+  ],
+  Valparaíso: [
+    "Valparaíso",
+    "Casablanca",
+    "Concón",
+    "Juan Fernández",
+    "Puchuncaví",
+    "Quintero",
+    "Viña del Mar",
+    "Isla de Pascua",
+    "Los Andes",
+    "Calle Larga",
+    "Rinconada",
+    "San Esteban",
+    "La Ligua",
+    "Cabildo",
+    "Papudo",
+    "Petorca",
+    "Zapallar",
+    "Quillota",
+    "Calera",
+    "Hijuelas",
+    "La Cruz",
+    "Nogales",
+    "San Antonio",
+    "Algarrobo",
+    "Cartagena",
+    "El Quisco",
+    "El Tabo",
+    "Santo Domingo",
+    "San Felipe",
+    "Catemu",
+    "Llay-Llay",
+    "Panquehue",
+    "Putaendo",
+    "Santa María",
+    "Quilpué",
+    "Limache",
+    "Olmué",
+    "Villa Alemana",
+  ],
+  "Metropolitana de Santiago": [
+    "Santiago",
+    "Cerrillos",
+    "Cerro Navia",
+    "Conchalí",
+    "El Bosque",
+    "Estación Central",
+    "Huechuraba",
+    "Independencia",
+    "La Cisterna",
+    "La Florida",
+    "La Granja",
+    "La Pintana",
+    "La Reina",
+    "Las Condes",
+    "Lo Barnechea",
+    "Lo Espejo",
+    "Lo Prado",
+    "Macul",
+    "Maipú",
+    "Ñuñoa",
+    "Pedro Aguirre Cerda",
+    "Peñalolén",
+    "Providencia",
+    "Pudahuel",
+    "Quilicura",
+    "Quinta Normal",
+    "Recoleta",
+    "Renca",
+    "San Joaquín",
+    "San Miguel",
+    "San Ramón",
+    "Vitacura",
+    "Puente Alto",
+    "Pirque",
+    "San José de Maipo",
+    "Colina",
+    "Lampa",
+    "Tiltil",
+    "San Bernardo",
+    "Buin",
+    "Calera de Tango",
+    "Paine",
+    "Melipilla",
+    "Alhué",
+    "Curacaví",
+    "María Pinto",
+    "San Pedro",
+    "Talagante",
+    "El Monte",
+    "Isla de Maipo",
+    "Padre Hurtado",
+    "Peñaflor",
+  ],
+  "O'Higgins": [
+    "Rancagua",
+    "Codegua",
+    "Coinco",
+    "Coltauco",
+    "Doñihue",
+    "Graneros",
+    "Las Cabras",
+    "Machalí",
+    "Malloa",
+    "Mostazal",
+    "Olivar",
+    "Peumo",
+    "Pichidegua",
+    "Quinta de Tilcoco",
+    "Rengo",
+    "Requínoa",
+    "San Vicente",
+    "Pichilemu",
+    "La Estrella",
+    "Litueche",
+    "Marchihue",
+    "Navidad",
+    "Paredones",
+    "San Fernando",
+    "Chépica",
+    "Chimbarongo",
+    "Lolol",
+    "Nancagua",
+    "Palmilla",
+    "Peralillo",
+    "Placilla",
+    "Pumanque",
+    "Santa Cruz",
+  ],
+  Maule: [
+    "Talca",
+    "Constitución",
+    "Curepto",
+    "Empedrado",
+    "Maule",
+    "Pelarco",
+    "Pencahue",
+    "Río Claro",
+    "San Clemente",
+    "San Rafael",
+    "Cauquenes",
+    "Chanco",
+    "Pelluhue",
+    "Curicó",
+    "Hualañé",
+    "Licantén",
+    "Molina",
+    "Rauco",
+    "Romeral",
+    "Sagrada Familia",
+    "Teno",
+    "Vichuquén",
+    "Linares",
+    "Colbún",
+    "Longaví",
+    "Parral",
+    "Retiro",
+    "San Javier",
+    "Villa Alegre",
+    "Yerbas Buenas",
+  ],
+  Ñuble: [
+    "Chillán",
+    "Bulnes",
+    "Chillán Viejo",
+    "El Carmen",
+    "Pemuco",
+    "Pinto",
+    "Quillón",
+    "San Ignacio",
+    "Yungay",
+    "Quirihue",
+    "Cobquecura",
+    "Coelemu",
+    "Ninhue",
+    "Portezuelo",
+    "Ránquil",
+    "Treguaco",
+    "San Carlos",
+    "Coihueco",
+    "Ñiquén",
+    "San Fabián",
+    "San Nicolás",
+  ],
+  Biobío: [
+    "Concepción",
+    "Coronel",
+    "Chiguayante",
+    "Florida",
+    "Hualqui",
+    "Lota",
+    "Penco",
+    "San Pedro de la Paz",
+    "Santa Juana",
+    "Talcahuano",
+    "Tomé",
+    "Hualpén",
+    "Lebu",
+    "Arauco",
+    "Cañete",
+    "Contulmo",
+    "Curanilahue",
+    "Los Álamos",
+    "Tirúa",
+    "Los Ángeles",
+    "Antuco",
+    "Cabrero",
+    "Laja",
+    "Mulchén",
+    "Nacimiento",
+    "Negrete",
+    "Quilaco",
+    "Quilleco",
+    "San Rosendo",
+    "Santa Bárbara",
+    "Tucapel",
+    "Yumbel",
+    "Alto Biobío",
+  ],
+  "La Araucanía": [
+    "Temuco",
+    "Carahue",
+    "Cunco",
+    "Curarrehue",
+    "Freire",
+    "Galvarino",
+    "Gorbea",
+    "Lautaro",
+    "Loncoche",
+    "Melipeuco",
+    "Nueva Imperial",
+    "Padre Las Casas",
+    "Perquenco",
+    "Pitrufquén",
+    "Pucón",
+    "Saavedra",
+    "Teodoro Schmidt",
+    "Toltén",
+    "Vilcún",
+    "Villarrica",
+    "Cholchol",
+    "Angol",
+    "Collipulli",
+    "Curacautín",
+    "Ercilla",
+    "Lonquimay",
+    "Los Sauces",
+    "Lumaco",
+    "Purén",
+    "Renaico",
+    "Traiguén",
+    "Victoria",
+  ],
+  "Los Ríos": [
+    "Valdivia",
+    "Corral",
+    "Lanco",
+    "Los Lagos",
+    "Máfil",
+    "Mariquina",
+    "Paillaco",
+    "Panguipulli",
+    "La Unión",
+    "Futrono",
+    "Lago Ranco",
+    "Río Bueno",
+  ],
+  "Los Lagos": [
+    "Puerto Montt",
+    "Calbuco",
+    "Cochamó",
+    "Fresia",
+    "Frutillar",
+    "Los Muermos",
+    "Llanquihue",
+    "Maullín",
+    "Puerto Varas",
+    "Castro",
+    "Ancud",
+    "Chonchi",
+    "Curaco de Vélez",
+    "Dalcahue",
+    "Puqueldón",
+    "Queilén",
+    "Quellón",
+    "Quemchi",
+    "Quinchao",
+    "Osorno",
+    "Puerto Octay",
+    "Purranque",
+    "Puyehue",
+    "Río Negro",
+    "San Juan de la Costa",
+    "San Pablo",
+    "Chaitén",
+    "Futaleufú",
+    "Hualaihué",
+    "Palena",
+  ],
+  Aysén: [
+    "Coyhaique",
+    "Lago Verde",
+    "Aysén",
+    "Cisnes",
+    "Guaitecas",
+    "Cochrane",
+    "O'Higgins",
+    "Tortel",
+    "Chile Chico",
+    "Río Ibáñez",
+  ],
+  Magallanes: [
+    "Punta Arenas",
+    "Laguna Blanca",
+    "Río Verde",
+    "San Gregorio",
+    "Cabo de Hornos",
+    "Antártica",
+    "Porvenir",
+    "Primavera",
+    "Timaukel",
+    "Natales",
+    "Torres del Paine",
+  ],
 };
 
 function Cotizador() {
@@ -39,20 +409,20 @@ function Cotizador() {
   const regiones = Object.keys(regionesComunas);
 
   const regionesFiltradas = regiones.filter((r) =>
-    r.toLowerCase().includes(busquedaRegion.toLowerCase())
+    r.toLowerCase().includes(busquedaRegion.toLowerCase()),
   );
 
   const todasLasComunas = regiones.flatMap((r) =>
     regionesComunas[r].map((c) => ({
       comuna: c,
       region: r,
-    }))
+    })),
   );
 
   const comunasFiltradas = todasLasComunas.filter((item) =>
     `${item.comuna} ${item.region}`
       .toLowerCase()
-      .includes(busquedaComuna.toLowerCase())
+      .includes(busquedaComuna.toLowerCase()),
   );
 
   function cambiarDato(e) {
@@ -84,52 +454,47 @@ function Cotizador() {
     setBusquedaComuna(item.comuna);
   }
 
-async function enviarCotizacionHandler(e) {
-  e.preventDefault();
+  async function enviarCotizacionHandler(e) {
+    e.preventDefault();
 
-  setError("");
+    setError("");
 
-  if (!formulario.id_seguro) {
-    setError("Selecciona un tipo de seguro.");
-    return;
+    if (!formulario.id_seguro) {
+      setError("Selecciona un tipo de seguro.");
+      return;
+    }
+
+    setEnviando(true);
+
+    try {
+      const data = {
+        id_seguro: Number(formulario.id_seguro),
+        nombre: formulario.nombre,
+        rut: formulario.rut,
+        tipo_cliente: formulario.tipo_cliente,
+        email: formulario.email,
+        telefono: formulario.telefono,
+        canal: "tradicional",
+        mensaje: formulario.mensaje || "",
+        datos_adicionales: {
+          region: formulario.region || "",
+          comuna: formulario.comuna || "",
+        },
+      };
+
+      console.log("DATA ENVIADA:", data);
+
+      await enviarCotizacion(data);
+
+      navigate("/cotizacion-exitosa");
+    } catch (error) {
+      console.error("ERROR COMPLETO:", error);
+
+      setError(error.message || "No se pudo enviar la solicitud.");
+    } finally {
+      setEnviando(false);
+    }
   }
-
-  setEnviando(true);
-
-  try {
-    const data = {
-      id_seguro: Number(formulario.id_seguro),
-      nombre: formulario.nombre,
-      rut: formulario.rut,
-      tipo_cliente: formulario.tipo_cliente,
-      email: formulario.email,
-      telefono: formulario.telefono,
-      canal: "tradicional",
-      mensaje: formulario.mensaje || "",
-      datos_adicionales: {
-        region: formulario.region || "",
-        comuna: formulario.comuna || "",
-      },
-    };
-
-    console.log("DATA ENVIADA:", data);
-
-    await enviarCotizacion(data);
-
-    navigate("/cotizacion-exitosa");
-
-  } catch (error) {
-    console.error("ERROR COMPLETO:", error);
-
-    setError(
-      error.message ||
-      "No se pudo enviar la solicitud."
-    );
-
-  } finally {
-    setEnviando(false);
-  }
-}
 
   return (
     <>
@@ -307,9 +672,15 @@ async function enviarCotizacionHandler(e) {
             <ul>
               <li>Recibimos tu solicitud de cotización.</li>
               <li>Analizamos las mejores opciones según tus necesidades.</li>
-              <li>Un asesor de Prieto & Correa se pondrá en contacto contigo.</li>
-              <li>Te acompañamos para resolver dudas y comparar alternativas.</li>
-              <li>Recibes una propuesta personalizada para tomar tu decisión.</li>
+              <li>
+                Un asesor de Prieto & Correa se pondrá en contacto contigo.
+              </li>
+              <li>
+                Te acompañamos para resolver dudas y comparar alternativas.
+              </li>
+              <li>
+                Recibes una propuesta personalizada para tomar tu decisión.
+              </li>
             </ul>
           </div>
         </div>
